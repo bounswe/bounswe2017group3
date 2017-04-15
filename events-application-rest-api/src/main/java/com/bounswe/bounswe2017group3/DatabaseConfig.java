db23c0e733116d3d7264cb6d63a4802761cc943c
Samed Duzcay <samedduzcay@gmail.com>
2017-04-15 23:55:52 +0300

    Implement our PostgreSQL database configuration

---
+++ events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package com.bounswe.bounswe2017group3;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+}
