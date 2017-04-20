168998bb8acf4cd87d57fd7203f63c6f5b646b0a
Samed Duzcay <samedduzcay@gmail.com>
2017-04-20 23:53:19 +0300

    Add a home controller to render some meta data on API root.

---
+++ events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/HomeController.java
@@ -0,0 +1,28 @@
+package com.bounswe.bounswe2017group3;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+    @RequestMapping(method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody Map<String, String> home(ModelMap model) {
+        return new HashMap<String, String>() {
+            private static final long serialVersionUID = 7475539268535723836L;
+            {
+                put("version", "1.0.0");
+                put("git", "https://github.com/bounswe/bounswe2017group3.git");
+                put("baseUrl", "https://bounswe2017group3.herokuapp.com/");
+            }
+        };
+    }
+}
