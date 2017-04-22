9b0be32a73b13766a9cd4841c44ff9c8dbd04b4b
gokceuludogan <gokceuludogan@gmail.com>
2017-04-22 15:51:18 +0300

    Add methods with Sql queries to get the occurrences of given parameter

--- events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/UserRepository.java
+++ events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/UserRepository.java
@@ -1,13 +1,21 @@
 package com.bounswe.bounswe2017group3;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
-
+        
     public User findByUsername(String username);
 
-    public User findById(long    id);
+    public User findById(long id);
+
+    @Query("SELECT COUNT(u) FROM User u where u.username = :username") 
+    public int numberOfUsersWithUsername(@Param("username") String username);
+    
+    @Query("SELECT COUNT(u) FROM User u where u.username = :email") 
+    public int numberOfUsersWithEmail(@Param("email") String email);   
 }
