0bd0be9f775b2f6191fa4a78adf433248dc10dbf
gokceuludogan <gokceuludogan@gmail.com>
2017-04-22 13:52:05 +0300

    Fix findById method of UserRepository

--- events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/UserRepository.java
+++ events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/UserRepository.java
@@ -9,5 +9,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     public User findByUsername(String username);
 
-    public User findById(String id);
+    public User findById(long    id);
 }
