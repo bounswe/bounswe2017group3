4341bfd7c4e37599189395f38a6cb1ec3f7eee42
gokceuludogan <gokceuludogan@gmail.com>
2017-04-22 19:54:49 +0300

    Add exception handling

--- events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/UserController.java
+++ events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/UserController.java
@@ -24,11 +24,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.validation.Valid;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -57,21 +58,72 @@ public class UserController {
     public @ResponseBody List<User> user(ModelMap model) {
         return repository.findAll();
     }
-     
-    @RequestMapping(method=RequestMethod.PUT, value="{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-     public @ResponseBody User update(@PathVariable long id, ModelMap model,
-        @ModelAttribute("updateUser") @Valid User user,
-        BindingResult result) {
-        if (!result.hasErrors() && repository.numberOfUsersWithUsername(user.getUsername()) == 0 && repository.numberOfUsersWithEmail(user.getEmail()) == 0) {
-            User update = repository.findById(id);
-            update.setEmail(user.getEmail());
-            update.setFullname(user.getFullname());
-            update.setUsername(user.getUsername());
-            return repository.save(update); 
-        }
-        return user;
-     }
     
+    
+    @RequestMapping(method=RequestMethod.PUT, value="/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+      public @ResponseBody User update(@PathVariable("id") long id,  @RequestParam MultiValueMap<String, String> params) {
+         
+            User update = repository.findById(id);
+            String newUsername = params.get("username").get(0);
+            String newEmail = params.get("email").get(0);
+            String newFullname = params.get("fullname").get(0);
+            int count =repository.numberOfUsersWithEmail(newEmail);
+            System.out.println(count);
+            if(count == 0 || newEmail.equalsIgnoreCase(update.getEmail())){
+                update.setEmail(newEmail);
+            }else{
+                throw new CustomException("1001","Given email address belongs to someone else.");
+            }
+            update.setFullname(newFullname);
+            count = repository.numberOfUsersWithUsername(newUsername);
+            System.out.println(count);
+            if(count == 0 || newUsername.equalsIgnoreCase(update.getUsername())){
+                
+                update.setUsername(newUsername);
+            }else{
+                throw new CustomException("1002","Given username belongs to someone else.");
+            }
+
+            return repository.save(update); 
+             
+             /*update.setEmail(user.getEmail());
+             update.setFullname(user.getFullname());
+             update.setUsername(user.getUsername());
+             return repository.save(update); 
+        */ 
+         //System.out.println("sadfds");
+         //return user;
+          
+      } 
+    /*
+    @RequestMapping(method=RequestMethod.PUT, value="/{id}",
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+     public @ResponseBody User update(@PathVariable("id") long id, @RequestParam MultiValueMap<String, String> params) {
+        System.out.println(id);
+        System.out.println(repository.findById(id));
+        User update = repository.findById(id);
+        System.out.println(update.toString());
+        System.out.println(params.toString());
+        String newUsername = params.get("username").get(0);
+        String newEmail = params.get("email").get(0);
+        String newFullname = params.get("fullname").get(0);
+        if(repository.numberOfUsersWithEmail(newEmail) == 0 || newEmail == update.getEmail()){
+            update.setEmail(newEmail);
+        }else{
+            throw new CustomException("1001","Given email address belongs to someone else.");
+        }
+        update.setFullname(newFullname);
+        if(repository.numberOfUsersWithUsername(newUsername) == 0 || newUsername == update.getUsername()){
+            update.setUsername(newUsername);
+        }else{
+            throw new CustomException("1002","Given username belongs to someone else.");
+        }
+
+        return repository.save(update); 
+
+        
+     }
+      */ 
     @RequestMapping(method=RequestMethod.PUT,
             value="",
             params="username",
@@ -80,20 +132,23 @@ public class UserController {
         
         String oldUsername = params.get("username").get(0);
         String newUsername = params.get("username").get(1);
-        if(repository.numberOfUsersWithUsername(newUsername) == 0 && repository.numberOfUsersWithEmail(params.get("email").get(0)) == 0){
-            User updatedUser = repository.findByUsername(oldUsername);
-            updatedUser.setEmail(params.get("email").get(0));
-            updatedUser.setFullname(params.get("fullname").get(0));
-            updatedUser.setUsername(newUsername);
-            return repository.save(updatedUser);
+        String newEmail = params.get("email").get(0);
+        User updatedUser = repository.findByUsername(oldUsername);
+        if(repository.numberOfUsersWithEmail(newEmail) == 0 || newEmail.equalsIgnoreCase(updatedUser.getEmail())){
+                updatedUser.setEmail(newEmail);
         }else{
-           return null;
+              throw new CustomException("1001","Given email address belongs to someone else.");
+        }                   
+        updatedUser.setFullname(params.get("fullname").get(0));
+        if(repository.numberOfUsersWithUsername(newUsername) == 0 || newUsername.equalsIgnoreCase(oldUsername)){
+               updatedUser.setUsername(newUsername);
+        }else{
+               throw new CustomException("1002","Given username belongs to someone else.");
         }
-        
-
-    }
-    
+       
+        return repository.save(updatedUser);
    
+    }
 
     @RequestMapping(method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -106,4 +161,13 @@ public class UserController {
         // TODO: Show an error when the result has errors.
         return user;
     }
+    
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> exceptionHandler(Exception ex) {
+            ErrorResponse error = new ErrorResponse();
+            //error.setErrorCode(101);
+            error.setMessage(ex.getMessage());
+        return new ResponseEntity<ErrorResponse>(error, HttpStatus.OK);
+    }
 }
+
