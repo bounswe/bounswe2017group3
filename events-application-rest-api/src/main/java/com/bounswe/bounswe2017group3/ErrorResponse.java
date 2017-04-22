e51ea17a6baf8810f907c1fdf23fc8d24b501911
gokceuludogan <gokceuludogan@gmail.com>
2017-04-22 20:04:54 +0300

    Add classes for exception handling

---
+++ events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/ErrorResponse.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.bounswe.bounswe2017group3;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author gokce
+ */
+
+@Table(name = "errors")
+@Entity
+public class ErrorResponse implements Serializable {
+        @Id
+	private int errorCode;
+	private String message;
+	
+	public int getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
