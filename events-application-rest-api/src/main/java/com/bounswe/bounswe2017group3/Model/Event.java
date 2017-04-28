5f63ce6865e8b0d805d3406426db6f90ff0e24b3
esra-aydemir <esra.arastm@gmail.com>
2017-04-28 23:04:19 +0300

    Event files

---
+++ events-application-rest-api/src/main/java/com/bounswe/bounswe2017group3/Model/Event.java
@@ -0,0 +1,138 @@
+package com.bounswe.bounswe2017group3;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Table(name = "events")
+@Entity
+public class Event implements Serializable {
+    /**
+     * Unique serial identifier for the current version of the class.
+     */
+
+    /**
+     * Auto-generated id of the event.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    /**
+     * Name of the event.
+     */
+    @NotEmpty
+    @Column(unique = true)
+    private String name;
+
+    /**
+     * Description of the event.
+     */
+    private String description;
+
+    /**
+     * Date of the event.
+     */
+    private Date date;
+
+    /**
+     * Location of the event.
+     */
+    private String location;
+
+    /**
+     * Returns the id of the event.
+     *
+     * @ return Id of the event.
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Returns the name of the event.
+     *
+     * @return name of the event.
+     */
+    public String getname() {
+        return name;
+    }
+
+    /**
+     * Sets the name of the event.
+     *
+     * @param name Name of the event.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * Returns the description of the event.
+     *
+     * @return Description of the event.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description of the event.
+     *
+     * @param description Description of the event.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    /**
+     * Returns the location of the event.
+     *
+     * @return location of the event.
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * Sets the location of the event.
+     *
+     * @param location location of the event.
+     */
+    public void setLocation(String location) {
+        this.location =location;
+    }
+
+    /**
+     * Returns the date of the event.
+     *
+     * @return date of the event.
+     */
+    public Date getDate(){ return date;}
+
+    /**
+     * Sets the date od the event.
+     * @param date date of the event.
+     */
+    public void setDate(Date date) {this.date = date;}
+
+    /**
+     * Returns the string representation of the event object.
+     *
+     * @return String representation of the event object.
+     */
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", date='" + date + '\'' +
+                ", location='" + location + '\'' +
+                '}';
+    }
+}
