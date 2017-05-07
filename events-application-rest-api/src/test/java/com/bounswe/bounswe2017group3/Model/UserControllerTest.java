11c63ef9fb3f1eb071b6e70e3aeb5493be5610ab
gokceuludogan <gokceuludogan@gmail.com>
2017-05-08 01:34:20 +0300

    Change the test with mocked ones

--- events-application-rest-api/src/test/java/com/bounswe/bounswe2017group3/Model/UserControllerTest.java
+++ events-application-rest-api/src/test/java/com/bounswe/bounswe2017group3/Model/UserControllerTest.java
@@ -3,75 +3,80 @@ import com.bounswe.bounswe2017group3.Application;
 import com.bounswe.bounswe2017group3.Model.User;
 import com.bounswe.bounswe2017group3.Controller.UserController;
 import com.bounswe.bounswe2017group3.Repository.UserRepository;
-import com.jayway.restassured.RestAssured;
-import static com.jayway.restassured.RestAssured.when;
-import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
-import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Arrays;
-import org.apache.http.HttpStatus;
-import org.hamcrest.Matchers;
+import java.util.List;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = Application.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class UserControllerTest {
- 
-    @Autowired   
-    UserRepository repository;
-    User usr1;
-    User usr2;
-    User usr3;
-    @Value("${local.server.port}")   
-    int port;
- 
+    MockMvc mockMvc;
+    @Mock
+    UserRepository repo;
+    @InjectMocks
+    UserController userController;
     @Before
-    public void setUp() {
+    public void init(){
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(userController)
+                .build();
+    }
+    /**
+     * Tests whether GET /user endpoint works or not
+     * @throws Exception 
+     */
+    @Test
+    public void test_get_all_success() throws Exception {
+        List<User> users = Arrays.asList(
+                new User("gokce","Gökçe Uludoğan", "gokce@gmail.com","123123"),
+                new User("samed","Samed Düzçay","smddzcy@gmail.com","123456"));
+        Mockito.when(repo.findAll()).thenReturn(users);
+
+        mockMvc.perform(get("/user"))
+                .andExpect(status().isOk())
+                .andExpect( content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect( jsonPath("$[0].username", is("gokce")))
+                .andExpect(jsonPath("$[0].fullname",is("Gökçe Uludoğan")))
+                .andExpect(jsonPath("$[0].email",is("gokce@gmail.com")))                      
+                .andExpect( jsonPath("$[1].username", is("samed")))
+                .andExpect(jsonPath("$[1].fullname",is("Samed Düzçay")))
+                .andExpect(jsonPath("$[1].email",is("smddzcy@gmail.com")));
         
-        usr1 = new User("gokce","Gökçe Uludoğan", "gokce@gmail.com","123123");
-        usr2 = new User("samed","Samed Düzçay","smddzcy@gmail.com","123456");
-        usr3 = new User("esra","Esra Aydemir","esra-aydemir@gmail.com","222333");        
-        repository.deleteAll();
-        repository.save(Arrays.asList(usr1,usr2,usr3));
-        RestAssuredMockMvc.standaloneSetup(new UserController(repository));
-        RestAssured.port = port;
-    }
- 
-    @Test
-    public void canGetByUsername() {
-        String username = usr1.getUsername();
-        given().
-                param("username","gokce").
-        when().
-                get("/user").
-        then().
-                statusCode(HttpStatus.SC_OK).
-                body("fullname", Matchers.is("Gökçe Uludoğan")).
-                body("email", Matchers.is("gokce@gmail.com"));
+        verify(repo, times(1)).findAll();
+        verifyNoMoreInteractions(repo);
     }
     
-    @Test
-    public void canGetAll() {
-        when().
-                get("/user").
-        then().
-                statusCode(HttpStatus.SC_OK).
-                body("username", Matchers.hasItems("gokce", "samed", "esra"));
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
-    
-    
-    @Test
-    public void canDeleteById() {
-        long usr3Id = usr3.getId();
-        when()
-                .delete("/user/{id}", usr3Id).
-        then().
-                statusCode(HttpStatus.SC_NO_CONTENT);
-    }
+
 }
