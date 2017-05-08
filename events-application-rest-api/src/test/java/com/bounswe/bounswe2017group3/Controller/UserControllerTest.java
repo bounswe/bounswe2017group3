bb713d327a14b9d33265367205c235ad3b535659
Samed Duzcay <samedduzcay@gmail.com>
2017-05-08 15:14:27 +0300

    Fix packge of UserControllerTest

--- events-application-rest-api/src/test/java/com/bounswe/bounswe2017group3/Controller/UserControllerTest.java
+++ events-application-rest-api/src/test/java/com/bounswe/bounswe2017group3/Controller/UserControllerTest.java
@@ -1,33 +1,30 @@
-package com.bounswe.bounswe2017group3.Model;
+package com.bounswe.bounswe2017group3.Controller;
 import com.bounswe.bounswe2017group3.Application;
 import com.bounswe.bounswe2017group3.Model.User;
-import com.bounswe.bounswe2017group3.Controller.UserController;
 import com.bounswe.bounswe2017group3.Repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Arrays;
-import java.util.List;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = Application.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
